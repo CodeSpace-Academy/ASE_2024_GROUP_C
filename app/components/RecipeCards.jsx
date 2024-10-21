83213f4d797b564757fc9510ac26c625cdbf7f30
SABELOMAWELA <Mawelas63@gmail.com>
2024-10-21 23:22:18 +0200

    "Added servings information to RecipeCard component and reformatted layout"

--- app/components/RecipeCards.jsx
+++ app/components/RecipeCards.jsx
@@ -4,20 +4,25 @@ import Link from 'next/link';
 const RecipeCard = ({ recipe }) => {
   return (
     <Link href={`/recipes/${recipe.id}`}>
-    
-        <div className="bg-white shadow-md rounded-lg overflow-hidden">
-          <img src={recipe.image} alt={recipe.title} className="w-full h-48 object-cover" />
-          <div className="p-4">
-            <h2 className="text-xl font-bold text-gray-800">{recipe.name}</h2>
-            <p className="text-gray-600 mt-2">Prep time: {recipe.prepTimeMinutes} mins</p>
-            <p className="text-gray-600">Cook time: {recipe.cookTimeMinutes} mins</p>
-            <div className="flex items-center mt-2">
+      <div className="bg-white shadow-md rounded-lg overflow-hidden">
+        <img src={recipe.image} alt={recipe.title} className="w-full h-48 object-cover" />
+        <div className="p-4">
+          <h2 className="text-xl font-bold text-gray-800">{recipe.name}</h2>
+          <p className="text-gray-600 mt-2">Prep time: {recipe.prepTimeMinutes} mins</p>
+          <p className="text-gray-600">Cook time: {recipe.cookTimeMinutes} mins</p>
+
+        
+          <div className="flex items-center justify-between mt-2">
+            <div className="flex items-center">
               <span className="text-yellow-500">★</span>
               <span className="ml-2 text-gray-800">{recipe.rating}</span>
             </div>
+            <div className="text-gray-600">
+              Servings: {recipe.servings}
+            </div>
           </div>
         </div>
-     
+      </div>
     </Link>
   );
 };
