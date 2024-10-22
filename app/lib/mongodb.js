d451619cb1e3d4c45712b1d70dbfea6984267722
Kitso Mogale <kitsomogale19@gmail.com>
2024-10-22 22:36:20 +0200

    uri env file position fixed

--- app/lib/mongodb.js
+++ app/lib/mongodb.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = 'mongodb+srv://group-c:etKnp1R6OOkmgEjA@cluster0.rfl6z.mongodb.net/';
-//console.log(process.env.MONGODB_URI)
+const MONGODB_URI = process.env.MONGODB_URI;
+console.log(process.env.MONGODB_URI)
 if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
 }
